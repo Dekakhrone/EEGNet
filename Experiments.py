1fd465b56b81c51e4c5717dae6ab35d8bcb1f5dc
Anton <drobyshev.anton@ashmanov.net>
2020-03-29 22:42:07 +0300

    Using the Optuna package in experiments now

--- Experiments.py
+++ Experiments.py
@@ -1,68 +1,140 @@
 import os
-from random import randint
+import yaml
 
+import optuna
 import numpy as np
 import tensorflow as tf
 from tensorflow.keras.callbacks import ModelCheckpoint
 
-from Utils.DataLoader import DataHandler, Formats, permutate
 from Model import EEGNet
+from Utils.DataLoader import DataHandler, Formats, splitDataset
+from Utils.Metrics import ROC
 
 
 gpu = tf.config.experimental.list_physical_devices('GPU')[0]
 tf.config.experimental.set_memory_growth(gpu, True)
 
 
-def splitDataset(data, labels, trainPart=0.8, valPart=0.2, permutation=True):
-	assert labels.shape[0] == data.shape[0]
-	assert trainPart + valPart <= 1
+class OptunaTrainer:
+	def __init__(self, config, dataset):
+		config = self._loadConfig(config)
 
-	length = labels.shape[0]
-	testPart = 1.0 - trainPart - valPart
+		self.checkpointPath = config["paths"]["checkpoint"]
+		self.logPath = config["paths"]["logs"]
 
-	partList = ["train", "val", "test"]
-	parts = {"train": trainPart, "val": valPart, "test": testPart}
+		self.epochs = config["train_config"]["epochs"]
+		self.batchsize = config["train_config"]["batchsize"]
 
-	dataset = {}
-	start = 0
-	for part in partList:
-		ratio = parts.get(part)
-		end = start + round(ratio * length)
+		self.dataset = dataset
 
-		seed = randint(0, 2**16)
 
-		dataset[part] = (
-			data[start:end] if not permutation else permutate(data[start:end], saveOrder=True, seedValue=seed),
-			labels[start:end] if not permutation else permutate(labels[start:end], saveOrder=True, seedValue=seed)
+	def __call__(self, trial):
+		shape = self.dataset["train"][0].shape[-2:]
+		model = self.buildModel(trial, shape)
+
+		checkpointPath = train(
+			model=model,
+			dataset=self.dataset,
+			weigthsPath=self.checkpointPath,
+			logPath=self.logPath,
+			epochs=self.epochs,
+			batchsize=self.batchsize
 		)
 
-		start = end
+		auc = test(model, checkpointPath, self.dataset["test"])
 
-	return dataset
+		return auc
 
 
-def train(model, dataset, weigthsPath, logPath, trainPart=0.8, validPart=0.2, epochs=100, batchsize=128):
+	@staticmethod
+	def _loadConfig(configSource):
+		if isinstance(configSource, dict):
+			return configSource
+		elif isinstance(configSource, str):
+			pass
+		else:
+			raise TypeError
+
+		with open(configSource, "r", encoding="utf-8") as stream:
+			config = yaml.safe_load(stream)
+
+		assert config is not None
+
+		return config
+
+
+	@staticmethod
+	def chooseOptimizer(trial):
+		kwargs = {}
+		optimizer_options = ["RMSprop", "Adam"]
+
+		optimizer_selected = trial.suggest_categorical("optimizer", optimizer_options)
+
+		if optimizer_selected == "RMSprop":
+			kwargs["learning_rate"] = trial.suggest_loguniform("rmsprop_learning_rate", 1e-5, 1e-1)
+			kwargs["decay"] = trial.suggest_uniform("rmsprop_decay", 0.85, 0.99)
+			kwargs["momentum"] = trial.suggest_loguniform("rmsprop_momentum", 1e-5, 1e-1)
+		elif optimizer_selected == "Adam":
+			kwargs["learning_rate"] = trial.suggest_loguniform("adam_learning_rate", 1e-5, 1e-1)
+
+		optimizer = getattr(tf.optimizers, optimizer_selected)(**kwargs)
+
+		return optimizer
+
+
+	def buildModel(self, trial, shape):
+		samples = shape[-1]
+		assert samples // 2 > 16
+
+		temporalLength = int(trial.suggest_discrete_uniform("temporal_length", 16, samples // 2, 4))
+		dropoutRate = trial.suggest_discrete_uniform("dropout_rate", 0.1, 0.5, 0.1)
+		D = trial.suggest_int("D", 1, 4)
+		poolKernel = int(trial.suggest_discrete_uniform("pool_kernel", 4, 16, 2))
+
+		model = EEGNet(
+			categoriesN=2,
+			electrodes=shape[0],
+			samples=shape[1],
+			temporalLength=temporalLength,
+			dropoutRate=dropoutRate,
+			D=D,
+			poolPad="same",
+			poolKernel=poolKernel
+		)
+
+		optimizer = self.chooseOptimizer(trial)
+
+		model.compile(
+			loss="sparse_categorical_crossentropy",
+			optimizer=optimizer,
+			metrics=["accuracy"]
+		)
+
+		return model
+
+
+def test(model, checkpointPath, dataset):
+	model.load_weights(checkpointPath)
+
+	data, labels = dataset
+
+	pred = model.predict(data)[:, 1]
+	auc = ROC(labels, pred)
+
+	return auc
+
+
+def train(model, dataset, weigthsPath, logPath, epochs=100, batchsize=128):
 	for path in [weigthsPath, logPath]:
 		os.makedirs(path, exist_ok=True)
 
 	checkpointPath = os.path.join(weigthsPath, "best.h5")
 	checkpointer = ModelCheckpoint(filepath=checkpointPath, verbose=1, save_best_only=True)
 
-	dataset = splitDataset(*dataset, trainPart=trainPart, valPart=validPart, permutation=True)
-
-	# dataTrain, labelsTrain = dataset["train"]
-
 	model.fit(*dataset["train"], batch_size=batchsize, epochs=epochs, verbose=2, validation_data=dataset["val"],
 	          callbacks=[checkpointer])
 
-	model.load_weights(checkpointPath)
-
-	dataTest, labelsTest = dataset["test"]
-
-	probs = model.predict(dataTest)
-	preds = probs.argmax(axis=-1)
-	acc = np.mean(preds == labelsTest)
-	print("Classification accuracy: %f " % (acc))
+	return checkpointPath
 
 
 def main():
@@ -76,27 +148,28 @@ def main():
 		shuffle=True
 	)
 
-	shape = data.shape
 	data = np.expand_dims(data, axis=1)
 
-	model = EEGNet(
-		categoriesN=2,
-		electrodes=shape[1],
-		samples=shape[2]
-	)
+	dataset = splitDataset(data, labels, trainPart=0.8, valPart=0.1, permutation=True, seed=42069)
 
-	model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	optunaTrainer = OptunaTrainer(config="config.yml", dataset=dataset)
 
-	train(
-		model=model,
-		dataset=(data, labels),
-		weigthsPath="./Data/Experiments",
-		logPath="./Data/Experiments/Logs",
-		trainPart=0.7,
-		validPart=0.1,
-		epochs=100,
-		batchsize=128
-	)
+	study = optuna.create_study(direction="maximize")
+	study.optimize(optunaTrainer, n_trials=10)
+
+	print("Number of finished trials: ", len(study.trials))
+
+	print("Best 5 trials:")
+	trials = study.trials
+	trials = sorted(trials, key=lambda elem: elem.value, reverse=True)[:5]
+
+	for i, trial in enumerate(trials):
+		print("Trial %d" % i)
+		print("\tValue: ", trial.value)
+
+		print("\tParams: ")
+		for key, value in trial.params.items():
+			print("\t\t{}: {}".format(key, value))
 
 
 if __name__ == "__main__":
---
+++ Utils/Metrics.py
@@ -0,0 +1,32 @@
+import os
+
+from sklearn.metrics import roc_curve, auc
+from matplotlib import pyplot as plt
+
+
+def ROC(true, pred, show=False, wpath=None, name=None):
+	FPR, TPR, thresh = roc_curve(true, pred)
+	AUC = auc(FPR, TPR)
+	
+	if show or wpath is not None:
+		plt.title("ROC")
+		plt.plot(FPR, TPR, "b", label="AUC = %0.2f" % AUC)
+		plt.legend(loc="lower right")
+		plt.plot([0, 1], [0, 1], "r--")
+		plt.xlim([0, 1])
+		plt.ylim([0, 1])
+		plt.ylabel("True Positive Rate")
+		plt.xlabel("False Positive Rate")
+		
+		if show:
+			plt.show()
+			
+		if wpath is not None:
+			os.makedirs(wpath, exist_ok=True)
+			
+			name = name if name is not None else "ROC_curve"
+			name = os.path.splitext(name)[0] + ".png"
+			
+			plt.savefig(os.path.join(wpath, name))
+			
+	return AUC
